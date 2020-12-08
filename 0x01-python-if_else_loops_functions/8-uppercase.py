6f1c1d2fd1c54b4863ae876fb49ee78beceac089
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-12-08 16:58:33 -0300

    fix pep8 format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for letter in str:
-        if ord(letter) >= ord("a") and ord(letter) < ord("z"):
+        if ord(letter) >= ord("a") and ord(letter) <= ord("z"):
             letter = chr(ord(letter) - 32)
         print("{:s}".format(letter), end="")
     print()
