3f3b5ba1f9a939fdd94fdb4101309b24b9da8ae0
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-12-10 14:05:19 -0300

    Fix file 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def print_format(operator, first, second, calc):
-    print("{} {} {} = {}".format(first, operator, second, calc(first, second)))
-
-
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
+
+    def print_format(operator, first, second, calc):
+        print("{} {} {} = {}".format(first, operator, second, calc(first, second)))
+
     a = 10
     b = 5
     print_format(operator="+", first=a, second=b, calc=add)
