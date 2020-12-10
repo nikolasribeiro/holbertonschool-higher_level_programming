5dfa0e31779c30aca009047f1beccbec75d6fb97
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-12-10 14:08:56 -0300

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,19 +2,13 @@
 if __name__ == "__main__":
     from sys import argv
 
-    def print_on_console(validator, count, formatter=""):
-        if validator:
-            print("{}: {}".format(count, formatter))
-        else:
-            print("{} argument. ".format(count))
-
     total_arguments = len(argv) - 1
 
     if total_arguments == 0:
-        print_on_console(False, total_arguments)
+        print("0 arguments.")
     elif total_arguments == 1:
-        print_on_console(False, total_arguments)
+        print("1 argument:")
     else:
-        print_on_console(False, total_arguments)
+        print("{} arguments:".format(total_arguments))
     for i in range(total_arguments):
-        print_on_console(True, i+1, argv[i + 1])
+        print("{}: {}".format(i + 1, argv[i + 1]))
