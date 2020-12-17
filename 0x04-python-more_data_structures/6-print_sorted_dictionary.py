c1e87f1a0f5053c6fa7bce3611f0838c1db6e75b
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-12-17 14:14:00 -0300

    added shebang to file

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,2 +1,4 @@
-def print_sorted_dictionary(a_dictionary):
-    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary)::
+    [print("{}: {}".format(key, a_dictionary[key]))
+     for key in sorted(a_dictionary)]
