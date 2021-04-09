e61e89f4682ba5c3090d8a82fdade3031d11ea33
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2021-04-09 14:22:25 -0300

    commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
+        decoded = html.decode("utf-8")
     print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
